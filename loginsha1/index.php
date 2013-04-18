93ddae864a8b3dc28879f0787011a02df5d3f869
Miguel Rafael Esteban Martín <miguel@miDebian.(none)>
2013-04-18 11:52:22 +0200

    Versión inicial

---
+++ loginsha1/index.php
@@ -0,0 +1,113 @@
+<?
+/*
+ *      index.php
+ *      
+ *      Copyright 2013 Miguel Rafael Esteban Martín <miguel.esteban@logicaalternativa.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ 
+ */
+
+
+	session_start();
+	
+	// Se calcula la cadena aleatoria
+	$tokenSesion = rand(1,9) * 1000000 + rand(0,10) * rand(0, 10000) ;
+	// Se guarda en sesión
+	$_SESSION['tokenSesion'] = $tokenSesion;
+	
+	
+?>
+<html>
+<head>
+<title>[LogicaAlternativa.com] Ejemplo de securización acceso sha1</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script language="JavaScript" src="https://raw.github.com/kvz/phpjs/master/functions/xml/utf8_encode.js"></script>
+<script language="JavaScript" src="https://raw.github.com/kvz/phpjs/master/functions/strings/sha1.js"></script>
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function acceder(){
+
+	var contra = document.forms.formularioAcceso.contra.value + '';
+	
+	var token= sha1('<?=$tokenSesion?>' + contra);
+		
+	document.forms.formularioAcceso.contra.value = '';
+	document.forms.formularioAcceso.token.value = token;
+	
+	return true;
+}
+
+function sf(){
+
+	document.forms.formularioAcceso.usu.focus();
+	
+}
+-->
+</script>
+
+</head>
+
+<body onLoad="javaScript:sf();">
+<div align="center">
+	<p>[LogicaAlternativa.com] Ejemplo de securización acceso sha1</p>  
+</div>
+
+<br/>
+
+<form name="formularioAcceso" method="post" action="resultado.php" onSubmit="return acceder()">
+
+   <input name="token" type="hidden" value="">
+    
+  <table width="500" border="0" align="center" cellpadding="6" cellspacing="0">
+    <tr> 
+      <td><div align="right">Usuario:</div></td>
+      <td> <input name="usu" type="text" id="usu" value=""> </td>
+    </tr>
+    <tr> 
+      <td><div align="right">Contrase&ntilde;a:</div></td>
+      <td><input name="contra" type="password" id="contra"></td>
+    </tr>
+    <tr> 
+      <td colspan="2"><div align="center"> 
+          <input type="reset" name="Reset" value="Cancelar">
+          &nbsp 
+          <input name="entrar" type="submit" id="entrar" value="         Entrar         ">
+        </div></td>
+    </tr>
+  </table>
+  <br>
+    </form>
+   <br/>
+   <div align="center">
+  <p>Usuarios de prueba</p> 
+    <table width="0" border="0" cellpadding="6" cellspacing="0">
+    <tr> 
+      <th><div align="right">Usuario</div></th>
+      <th><div align="left">Contraseña</div></th>
+    </tr>
+    <tr> 
+      <td><div align="right">usu1</div></td>
+      <td><div align="left">contra1</div></td>
+    </tr>
+    <tr> 
+      <td><div align="right">usu1</div></td>
+      <td><div align="left">contra2</div></td>
+    </tr>
+	</table>
+ </div>
+</body>
+</html>
---
+++ loginsha1/resultado.php
@@ -0,0 +1,83 @@
+<?
+/*
+ *      resultado.php
+ *      
+ *      Copyright 2013 Miguel Rafael Esteban Martín <miguel.esteban@logicaalternativa.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+	session_start();
+	
+	require_once './lib/accesodummy.class.php';
+	
+	$usuario = $_REQUEST[ 'usu'];
+	$token = $_REQUEST['token'];
+	
+	$tokenSesion = $_SESSION['tokenSesion'];	
+	$_SESSION['tokenSesion'] = null;
+	
+	
+	// Usuarios	
+	$claseAcceso = new AccesoDummy();
+	
+	// Se obtiene la contraseña del usuario
+	$contra = $claseAcceso->dameContrasena( $usuario );
+	
+	// Se calcula el token
+	$tokenCalculado = sha1( $tokenSesion.$contra );	
+	
+	$texto='';
+	
+	if ( isset( $contra )  &&
+				$token == $tokenCalculado ){
+		
+		$texto = 'Acceso correcto';		
+		session_destroy();
+		session_start();
+		// Aquí se hace lo necesario obtener las propiedades del usuario y cargarlo en sesión
+		
+		
+	}else {
+		$texto = '<font color="#ff0000">Acceso incorrecto</font>';
+		
+	}
+	
+	
+?>
+<html>
+<head>
+<title>[LogicaAlternativa.com] Ejemplo de securización acceso sha1</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+</head>
+
+<body>
+	<div align="center">
+		<p>[LogicaAlternativa.com] Ejemplo de securización acceso sha1</p>  
+	</div>
+
+	<div align="center">
+	<br>
+	  <h2><?= $texto ?></h2>  
+	<h3>Datos que llegan del formulario</h3>
+	  <p>Usuario: <em><?= $usuario ?></em></p>
+	  <p>Token: <em><?= $token ?></em></p>
+	  <p>Contraseña: <em><?= $_REQUEST['contra'] ?></em></p> 
+	  <p><a href= "index.php">volver</a></p>
+	</div>
+</body>
+</html>
